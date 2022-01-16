717211e462539715b1ed8d98bf67666dcf21de08
mister-bee <thebeech@yahoo.com>
2022-01-16 16:02:10 -0300

    setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Based on JavaScript Mastery 
+# https://www.youtube.com/watch?v=Wn_Kb3MR_cU
---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Super Bueno FantasKrypto
+    </h1>
+
+  )
+}
+
+export default App
