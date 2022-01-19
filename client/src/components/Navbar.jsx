4ab2851e3d84e38336f69a2ca8dea154999d296f
mister-bee <thebeech@yahoo.com>
2022-01-19 01:45:26 -0300

    add useFetch, shortern util, and footer comp

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={ethereum2} alt="ethereum2" className="w-32 cursor-pointer" />
+        <img src={ethereum2} alt="ethereum2" className="w-16 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
@@ -47,30 +47,4 @@ const Navbar = () => {
 };
 
 export default Navbar;
-// import { HiMenuAlt4 } from 'react-icons/hi'
-// import { AiOutlineClose } from 'react-icons/ai'
-// 
 
-// const NavbarItem = ({ title, classProps }) => {
-//   return (
-//     <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>
-//   )
-// }
-
-
-// const Navbar = () => {
-//   return (
-//     <nav className="w-full flex md:justify-center justify-between items-center p-4">
-//       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-//         <img src={ethereum2} alt="ethereum-logo" className="w-32 cursor-pointer" />
-//       </div>
-//       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-//         {["Market", "Exchange", "Tutorials", "Wallters"].map((item, index) => (<NavbarItem key={item + index} title={item} />))}
-//         <li className="bg=[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]"></li>
-
-//       </ul>
-//     </nav>
-//   )
-// }
-
-// export default Navbar
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,10 +1,9 @@
 import React, { useContext } from "react";
-//import { AiFillPlayCircle } from "react-icons/ai";
+import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
 import { TransactionContext } from "../context/TransactionContext";
-//import { shortenAddress } from "../utils/shortenAddress";
+import { shortenAddress } from '../../utils/shortenAddress'
 import { Loader } from ".";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -47,7 +46,7 @@ const Welcome = () => {
               onClick={connectWallet}
               className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
             >
-              {/* <AiFillPlayCircle className="text-white mr-2" /> */}
+              <AiFillPlayCircle className="text-white mr-2" />
               <p className="text-white text-base font-semibold">
                 Connect Wallet
               </p>
@@ -84,7 +83,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  {/* {shortenAddress(currentAccount)} */}
+                  {shortenAddress(currentAccount)}
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
